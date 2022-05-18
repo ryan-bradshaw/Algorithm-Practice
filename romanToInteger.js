cd45c659f52f3d39f958d08aa2e2f13e31fca0a8
Ryan <rjamesbradshaw@gmail.com>
2022-05-18 14:38:53 -0700

    roman numeral to integer added

---
+++ romanToInteger.js
@@ -0,0 +1,59 @@
+// Given a roman numeral s, convert it to an integer.
+
+// Roman numerals are usually written largest to 
+// smallest from left to right. However, the numeral for four is not IIII. 
+// Instead, the number four is written as IV. 
+// Because the one is before the five we subtract it making four. 
+// The same principle applies to the number nine, which is written as IX. 
+// There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+
+// It is guaranteed that s is a valid roman numeral in the range [1, 3999]
+
+// Example 1:
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+
+// Example 2:
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+
+// Example 3:
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+
+let s = "IX";
+
+function romanToInteger(s){
+    let symbols = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    };
+    let value = 0;
+    for(let i = 0; i < s.length; i+=1){
+        symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]]: value += symbols[s[i]]
+    }
+    return value
+};
+
+console.log(romanToInteger(s));
